0cafadabfef1595a6c623e1539b3867ed37e236d
nileoe <linoperdrix@gmail.com>
2024-06-04 01:34:28 +0100

    changed hyprland config file

--- home-manager/homeModules/hypr/hyprland.nix
+++ home-manager/homeModules/hypr/hyprland.nix
@@ -1,6 +1,6 @@
 { config, pkgs, sessionSettings, ...}:
 {
-	programs.hyprland = {
+	wayland.windowManager.hyprland = {
 	enable = false;
 	};
 	# services.hypridle.enable = true; # try it some time!
@@ -8,11 +8,11 @@
 
 	# always sourcing the desktop config for now
 
-	home.file.".confi/hypr/hyprland.conf".source = ./desktopHyprland.conf;
-	home.file.".confi/hypr/binds.conf".source = ./binds.conf;
-	home.file.".confi/hypr/graphics.conf".source = ./graphics.conf;
-	home.file.".confi/hypr/startup.conf".source = ./startup.conf;
-	home.file.".confi/hypr/windowRules.conf".source = ./windowRules.conf;
-	home.file.".confi/hypr/dev-specific/monitor.conf".source = ./dev-specific/monitor-1440p144hz.conf;
-	home.file.".confi/hypr/dev-specific/dev-specific_binds.conf".source = ./dev-specific/g915-specific_binds.conf;
+	home.file.".config/hypr/hyprland.conf".source = ./desktop-hyprland.conf;
+	home.file.".config/hypr/binds.conf".source = ./binds.conf;
+	home.file.".config/hypr/graphics.conf".source = ./graphics.conf;
+	home.file.".config/hypr/startup.conf".source = ./startup.conf;
+	home.file.".config/hypr/windowRules.conf".source = ./windowRules.conf;
+	home.file.".config/hypr/dev-specific/monitor.conf".source = ./dev-specific/monitor-1440p144hz.conf;
+	home.file.".config/hypr/dev-specific/dev-specific_binds.conf".source = ./dev-specific/g915-specific_binds.conf;
 }
