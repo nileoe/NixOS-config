b8cc774ed9bca6ed4393b08296198e480c6396f8
nileoe <linoperdrix@gmail.com>
2024-07-08 11:04:19 +0100

    enable flake registery to use nix command normally and not configure those

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
     description = "lixing";
 
     inputs = {
-# Nixpkgs
+        # Nixpkgs
         nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
-# Home manager
+        # Home manager
         home-manager.url = "github:nix-community/home-manager/release-24.05";
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
         catppuccin.url = "github:catppuccin/nix";
@@ -34,26 +34,26 @@
     # hardwareConfiguration = import "${inputs.dev_specific_config}/hardwareConfiguration.nix"; # does not work for some reason
     in {
     # rest of the flake configuration
-# NixOS configuration entrypoint
-# Available through 'nixos-rebuild --flake .#your-hostname'
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
         nixosConfigurations = {
             lix = nixpkgs.lib.nixosSystem {
                 specialArgs = {inherit inputs outputs; inherit systemSettings; inherit userSettings; inherit sessionSettings;}; # inherit hardwareConfiguration; 
-# > Our main nixos configuration file <
+                # > Our main nixos configuration file <
                 modules = [./nixos/configuration.nix];
             };
         };
 
-# Standalone home-manager configuration entrypoint
-# Available through 'home-manager --flake .#your-username@your-hostname'
+        # Standalone home-manager configuration entrypoint
+        # Available through 'home-manager --flake .#your-username@your-hostname'
         homeConfigurations = {
-# "nileoe@lix" = home-manager.lib.homeManagerConfiguration {
-    "${userSettings.username}@${systemSettings.hostname}" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-            extraSpecialArgs = {inherit inputs outputs; inherit userSettings; inherit systemSettings; inherit sessionSettings;};
-# > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+        # "nileoe@lix" = home-manager.lib.homeManagerConfiguration {
+            "${userSettings.username}@${systemSettings.hostname}" = home-manager.lib.homeManagerConfiguration {
+                pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+                    extraSpecialArgs = {inherit inputs outputs; inherit userSettings; inherit systemSettings; inherit sessionSettings;};
+                # > Our main home-manager configuration file <
+                modules = [./home-manager/home.nix];
+            };
+        };
     };
-};
-};
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,14 +1,14 @@
 # credit to https://github.com/Misterio77/nix-starter-configs for the base structure
 {
     inputs,
-    lib,
-    config,
-    pkgs,
-    # hardwareConfiguration,
-    userSettings,
-    systemSettings,
-    sessionSettings,
-    ...
+        lib,
+        config,
+        pkgs,
+        # hardwareConfiguration,
+        userSettings,
+        systemSettings,
+        sessionSettings,
+        ...
 }:
 let
 tokyo-night-sddm = pkgs.libsForQt5.callPackage ./modules/ssdm-tokyonight-night/default.nix { };
@@ -20,152 +20,151 @@ in {
     services.displayManager.sddm.theme = "tokyo-night-sddm";
     environment.systemPackages = with pkgs; [
         tokyo-night-sddm
-        steam-run
-        ntfs3g
-        openssl
-        atkmm
-        librsvg
-        haskellPackages.webkit2gtk3-javascriptcore
-        ocamlPackages.lablgtk3-rsvg2
-        (import ./modules/scripts/startWpRotation.nix {inherit pkgs; })
-        (import ./modules/scripts/toggleWaybar.nix {inherit pkgs; })
-        (import ./modules/scripts/toggleWallpaper.nix {inherit pkgs; })
-        (import ./modules/scripts/changeWallpaper.nix {inherit pkgs; })
-        (import ./modules/scripts/batteryNotify.nix {inherit pkgs; })
-        (import ./modules/scripts/tmux-sessionizer.nix {inherit pkgs; })
+            steam-run
+            ntfs3g
+            openssl
+            atkmm
+            librsvg
+            haskellPackages.webkit2gtk3-javascriptcore
+            ocamlPackages.lablgtk3-rsvg2
+            (import ./modules/scripts/startWpRotation.nix {inherit pkgs; })
+            (import ./modules/scripts/toggleWaybar.nix {inherit pkgs; })
+            (import ./modules/scripts/toggleWallpaper.nix {inherit pkgs; })
+            (import ./modules/scripts/changeWallpaper.nix {inherit pkgs; })
+            (import ./modules/scripts/batteryNotify.nix {inherit pkgs; })
+            (import ./modules/scripts/tmux-sessionizer.nix {inherit pkgs; })
 
-# tmp
-        apacheHttpd
+            # tmp
     ];
     imports = [
         # hardwareConfiguration # does not work when .gitignoring (even uncommenting the relevant definition in flake.nix)
         /etc/nixos/hardware-configuration.nix
-        ./modules/sound-bluetooth-configuration.nix
-        # ./modules/steam.nix
-        ./modules/hyprlandAdditionalPackages.nix
-        ./modules/sql.nix
-        ./modules/cron.nix
-        # If you want to use modules from other flakes (such as nixos-hardware):
-        # inputs.hardware.nixosModules.common-cpu-amd
-        # inputs.hardware.nixosModules.common-ssd
-  ];
-  # rest of the config
-
-# TO ENABLE APPIMAGES AND SUCH generic dynamically linked executables
-  # programs.nix-ld = {
-  #   enable = true;
-  #   libraries = with pkgs ; [
-  #   ];
-  # };
-
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true; # uncomment for no dual-boot
-  boot.supportedFilesystems = [ "ntfs" ];
-  
-  i18n.defaultLocale = "en_GB.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-
-programs.hyprland.enable = true;
-
-gtk.iconCache.enable = true;
-
- fonts.packages = with pkgs; [
-	nerdfonts
-    noto-fonts
-    noto-fonts-color-emoji
-    corefonts
-    garamond-libre
-    helvetica-neue-lt-std
-    vistafonts
- ];
-
- environment.pathsToLink = [
- 	"/share/zsh" # to get completion for system packages (e.g. systemd)
- ];
-    services.libinput.enable = true;
-  time.timeZone = sessionSettings.timeZone;
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
+            ./modules/sound-bluetooth-configuration.nix
+            # ./modules/steam.nix
+            ./modules/hyprlandAdditionalPackages.nix
+            ./modules/sql.nix
+            ./modules/cron.nix
+            # If you want to use modules from other flakes (such as nixos-hardware):
+            # inputs.hardware.nixosModules.common-cpu-amd
+            # inputs.hardware.nixosModules.common-ssd
     ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
+    # rest of the config
+
+    # TO ENABLE APPIMAGES AND SUCH generic dynamically linked executables
+    # programs.nix-ld = {
+    #   enable = true;
+    #   libraries = with pkgs ; [
+    #   ];
+    # };
+
+    # boot.loader.systemd-boot.enable = true;
+    # boot.loader.efi.canTouchEfiVariables = true;
+
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.useOSProber = true; # uncomment for no dual-boot
+        boot.supportedFilesystems = [ "ntfs" ];
+
+    i18n.defaultLocale = "en_GB.UTF-8";
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_GB.UTF-8";
+        LC_IDENTIFICATION = "en_GB.UTF-8";
+        LC_MEASUREMENT = "en_GB.UTF-8";
+        LC_MONETARY = "en_GB.UTF-8";
+        LC_NAME = "en_GB.UTF-8";
+        LC_NUMERIC = "en_GB.UTF-8";
+        LC_PAPER = "en_GB.UTF-8";
+        LC_TELEPHONE = "en_GB.UTF-8";
+        LC_TIME = "en_GB.UTF-8";
     };
-  };
 
-  nix = let
-    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
-      flake-registry = "";
-      # Workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
+    programs.hyprland.enable = true;
+
+    gtk.iconCache.enable = true;
+
+    fonts.packages = with pkgs; [
+        nerdfonts
+            noto-fonts
+            noto-fonts-color-emoji
+            corefonts
+            garamond-libre
+            helvetica-neue-lt-std
+            vistafonts
+    ];
+
+    environment.pathsToLink = [
+        "/share/zsh" # to get completion for system packages (e.g. systemd)
+    ];
+    services.libinput.enable = true;
+    time.timeZone = sessionSettings.timeZone;
+    nixpkgs = {
+        # You can add overlays here
+        overlays = [
+            # If you want to use overlays exported from other flakes:
+            # neovim-nightly-overlay.overlays.default
+
+            # Or define it inline, for example:
+            # (final: prev: {
+            #   hi = final.hello.overrideAttrs (oldAttrs: {
+            #     patches = [ ./change-hello-to-hi.patch ];
+            #   });
+            # })
+        ];
+        # Configure your nixpkgs instance
+        config = {
+            # Disable if you don't want unfree packages
+            allowUnfree = true;
+        };
     };
-    # Opinionated: disable channels
-    channel.enable = false;
 
-    # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  };
+    nix = let
+        flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    in {
+        settings = {
+            # Enable flakes and new 'nix' command
+            experimental-features = "nix-command flakes";
+            # Opinionated: disable global registry?
+            # flake-registry = "";
+            # Workaround for https://github.com/NixOS/nix/issues/9574
+            nix-path = config.nix.nixPath;
+        };
+        # Opinionated: disable channels
+        channel.enable = false;
 
-  networking.hostName = "lix";
-networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
- environment.shells = with pkgs; [zsh ] ;  # prlbably not ?
- users.defaultUserShell = pkgs.zsh; # see if necessary
- programs.zsh.enable = true;
-
-  users.users.${userSettings.username} = {
-    isNormalUser = true;
-    description = userSettings.fullName;
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-      openssh.authorizedKeys.keys = [];
-  };
-
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Opinionated: forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
+        # Opinionated: make flake registry and nix path match flake inputs
+        registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+        nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
     };
-  };
-  services.nginx.package = pkgs.nginxStable.override { openssl = pkgs.libressl; };
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "24.05";
+    networking.hostName = "lix";
+    networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+        environment.shells = with pkgs; [zsh ] ;  # prlbably not ?
+        users.defaultUserShell = pkgs.zsh; # see if necessary
+        programs.zsh.enable = true;
+
+    users.users.${userSettings.username} = {
+        isNormalUser = true;
+        description = userSettings.fullName;
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [];
+        openssh.authorizedKeys.keys = [];
+    };
+
+    # This setups a SSH server. Very important if you're setting up a headless system.
+    # Feel free to remove if you don't need it.
+    services.openssh = {
+        enable = true;
+        settings = {
+            # Opinionated: forbid root login through SSH.
+            PermitRootLogin = "no";
+            # Opinionated: use keys only.
+            # Remove if you want to SSH using passwords
+            PasswordAuthentication = false;
+        };
+    };
+    services.nginx.package = pkgs.nginxStable.override { openssl = pkgs.libressl; };
+
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    system.stateVersion = "24.05";
 }
