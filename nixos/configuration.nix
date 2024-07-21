3b2f6494b7bd725b801a44edde91ed857758f9cb
nileoe <linoperdrix@gmail.com>
2024-07-21 14:22:15 +0100

    edited ssh conf

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -148,8 +148,6 @@ in {
         openssh.authorizedKeys.keys = [];
     };
 
-    # This setups a SSH server. Very important if you're setting up a headless system.
-    # Feel free to remove if you don't need it.
     services.openssh = {
         enable = true;
         settings = {
@@ -159,7 +157,6 @@ in {
             # Opinionated: use keys only.
             # Remove if you want to SSH using passwords
             # PasswordAuthentication = false;
-            PasswordAuthentication = true;
         };
     };
     # services.nginx.package = pkgs.nginxStable.override { openssl = pkgs.libressl; };
