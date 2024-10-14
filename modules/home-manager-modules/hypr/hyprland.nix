3670c785c49b6d2e0186248a3cde49e202ca04e8
nileoe <linoperdrix@gmail.com>
2024-10-14 13:20:06 +0100

    hypr sensible dev specific names

--- modules/home-manager-modules/hypr/hyprland.nix
+++ modules/home-manager-modules/hypr/hyprland.nix
@@ -11,8 +11,8 @@
             systemd.variables = ["--all"]; # try disabling: so that programs can be run outside of terminal (system needs to import environment)
 
             extraConfig = ''
-            source = ~/.config/hypr/dev-specific/monitor.conf
-            source = ~/.config/hypr/dev-specific/monitor-desktop.conf
+            source = ~/.config/hypr/dev-specific/monitor-specific.conf
+            source = ~/.config/hypr/dev-specific/keyboard-specific.conf
             source = ~/.config/hypr/windowRules.conf
             source = ~/.config/hypr/binds.conf
             source = ~/.config/hypr/graphics.conf
@@ -30,8 +30,8 @@
     # home.file.".config/hypr/hyprlock.conf".source = ./hyprlock.conf;
     # home.file.".config/hypr/hyprlockWallpaper.png".source = ./hyprlockWallpaper.png;
     # home.file.".config/hypr/hypridle.conf".source = ./hypridle.conf;
-    home.file.".config/hypr/dev-specific/monitor.conf".source = if userSettings.monitorType == "desktop" then ./dev-specific/monitor-desktop.conf else ./dev-specific/monitor-asusLaptop.conf;
-    home.file.".config/hypr/dev-specific/monitor-desktop.conf".source = if userSettings.keyboard == "fullSize" then ./dev-specific/binds-fullSize-specific.conf else ./dev-specific/binds-laptop-specific.conf;
+    home.file.".config/hypr/dev-specific/monitor-specific.conf".source = if userSettings.monitorType == "desktop" then ./dev-specific/monitor-desktop.conf else ./dev-specific/monitor-asusLaptop.conf;
+    home.file.".config/hypr/dev-specific/keyboard-specific.conf".source = if userSettings.keyboard == "fullSize" then ./dev-specific/binds-fullSize-specific.conf else ./dev-specific/binds-laptop-specific.conf;
 
 # services.hypridle.enable = true; # try it some time!
 # programs.hyprlock.enable = true; # try it some time!
