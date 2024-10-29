d26b15017d6589411660dd35e05fb1f6e895adb1
nileoe <linoperdrix@gmail.com>
2024-10-29 11:59:23 +0000

    minor windowrule anki change

--- modules/home-manager-modules/shell/sh.nix
+++ modules/home-manager-modules/shell/sh.nix
@@ -1,7 +1,11 @@
-{ config, pkgs, userSettings, ...}:
-let 
-shellAliases = {
-##### Basic commands / simple navigation
+{
+  config,
+  pkgs,
+  userSettings,
+  ...
+}: let
+  shellAliases = {
+    ##### Basic commands / simple navigation
     cat = "bat -p --theme 'Nord'";
     catt = "bat --theme 'Nord'";
     cl = "clear";
@@ -27,7 +31,7 @@ shellAliases = {
     tre = "eza -T";
     sl = "sl -e"; # :)
 
-##### Navigation and fzbvvbvbbvvv
+    ##### Navigation and fzbvvbvbbvvv
     cd = "z"; # zzzzzzzoxide
     ".1" = "cd ..";
     ".2" = ".1 && cd ..";
@@ -40,10 +44,11 @@ shellAliases = {
     fzb = "fzf --reverse --preview 'bat --style=numbers --color=always {}'";
     fzvb = "nvim $(fzb)";
     fzbv = "fzvb";
+    va = "nvim suppr.md"; # go into a temporary buffer to delete later
 
-##### NixOS
- #   hswitch= "cd ~/nix-config && home-manager switch --flake .#${userSettings.username}@${userSettings.hostname} --impure && notify-send 'home-manager switch complete.'";
- #   nswitch = "cd ~/nix-config && sudo nixos-rebuild switch --flake .#${userSettings.hostname} --impure && notify-send 'nixos-rebuilt switch complete'";
+    ##### NixOS
+    #   hswitch= "cd ~/nix-config && home-manager switch --flake .#${userSettings.username}@${userSettings.hostname} --impure && notify-send 'home-manager switch complete.'";
+    #   nswitch = "cd ~/nix-config && sudo nixos-rebuild switch --flake .#${userSettings.hostname} --impure && notify-send 'nixos-rebuilt switch complete'";
     switch = "cd ~/nixos/ && sudo nixos-rebuild switch --impure --flake .#${userSettings.hostname} && notify-send 'NixOS' 'Switch complete'";
     switcho = "switch && o";
     switchcd = "switch && cd && cl";
@@ -52,9 +57,8 @@ shellAliases = {
     vnix = "cd ~/nixos/ && nvim .";
     gho = "Hyprland";
 
-
-##### Miscellaneous
-# replace / remove as needed
+    ##### Miscellaneous
+    # replace / remove as needed
     airpods = "dispods; bluetoothctl connect 3C:4D:BE:89:38:4D && clear";
     airpodso = "(airpods &) && o";
     dispods = "bluetoothctl disconnect 3C:4D:BE:89:38:4D && clear";
@@ -64,90 +68,89 @@ shellAliases = {
     disboom = "bluetoothctl disconnect 10:94:97:2B:E5:39 && clear";
     disboomo = "(disboom &) && o";
 
-##### Git custom docs shortcuts, remove as needed
+    ##### Git custom docs shortcuts, remove as needed
     dpull = "cd ~/docs && git pull --rebase && cd ~";
     dpush = "cd ~/docs && git add * ; git commit -m '${userSettings.hostname} (${userSettings.monitorType}) commit' && git push";
 
-##### Coding shortcuts
+    ##### Coding shortcuts
     g = "git";
     G = "g";
     py = "python3";
-    p="clear && python3 *.py";
+    p = "clear && python3 *.py";
 
-    j="clear && javac *.java && clear && java Main && echo ''";
+    j = "clear && javac *.java && clear && java Main && echo ''";
 
-    rr="clear && cargo run";
-    rb="clear && cargo build";
-    rt="clear && cargo test";
+    rr = "clear && cargo run";
+    rb = "clear && cargo build";
+    rt = "clear && cargo test";
 
-    no="cl && node *.js";
+    no = "cl && node *.js";
 
-    mdb="mariadb -u lino -p";
+    mdb = "mariadb -u lino -p";
     ##### temp
     clicker = "sudo /home/nileoe/.cargo/bin/theclicker -l 274 --no-grab";
-};
-in
-{
-    programs = {
-        zsh =  {
-            enable = true;
-            inherit shellAliases;
-            enableCompletion = true;
-            autosuggestion.enable = true;
-            syntaxHighlighting.enable = true;
-            history.size = 10000;
+  };
+in {
+  programs = {
+    zsh = {
+      enable = true;
+      inherit shellAliases;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      history.size = 10000;
 
-            plugins = [
-            {
-                name = "powerlevel10k";
-                src = pkgs.zsh-powerlevel10k;
-                file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-            }
-            {
-                name = "powerlevel10k-config";
-                src = ./p10k-config;
-                file = "p10k.zsh";
-            }
-            ];
-            initExtra = ''
-	    bindkey '^o' autosuggest-accept
-	    function mkcd() { # creates a directory and cd into it in a single command.
-	        mkdir "$1"
-	            cd "$1"
-	    }
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = ./p10k-config;
+          file = "p10k.zsh";
+        }
+      ];
+      initExtra = ''
+        bindkey '^o' autosuggest-accept
+        function mkcd() { # creates a directory and cd into it in a single command.
+            mkdir "$1"
+                cd "$1"
+        }
 
-            function rpc() { # copies provided file's absolute path in clipboard (needs wl-copy)
-                realpath "$1" | wl-copy
-            }
+               function rpc() { # copies provided file's absolute path in clipboard (needs wl-copy)
+                   realpath "$1" | wl-copy
+               }
 
-            function v() {
-                nvim "$1" || nvim . 
-            }
+               function v() {
+                   nvim "$1" || nvim .
+               }
 
-	    # git add && git commit
-            function gac() {
-                git add . && git commit -m "$1"
-            }
-            function gacp() {
-                git add . && git commit -m "$1" && git push
-            }
+        # git add && git commit
+               function gac() {
+                   git add . && git commit -m "$1"
+               }
+               function gacp() {
+                   git add . && git commit -m "$1" && git push
+               }
 
-            source ${./zellij-basic-completion.zsh}
+               source ${./zellij-basic-completion.zsh}
 
-            export PATH=$PATH:$HOME/go/bin
-            export EDITOR="nvim"
-      export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
-                '';
-        };
-        bash =  {
-            enable = true;
-            inherit shellAliases;
-        };
-        fzf = {
-            enable = true;
-            enableZshIntegration = true;
-            enableBashIntegration = true;
-            tmux.enableShellIntegration = true;
-        };
+               export PATH=$PATH:$HOME/go/bin
+               export EDITOR="nvim"
+         export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
+      '';
     };
+    bash = {
+      enable = true;
+      inherit shellAliases;
+    };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      tmux.enableShellIntegration = true;
+    };
+  };
 }
