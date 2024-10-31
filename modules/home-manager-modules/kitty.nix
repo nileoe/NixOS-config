9f8c5122e6f4bd38e511e643521edb3ef78ea9d9
nileoe <linoperdrix@gmail.com>
2024-10-31 15:52:11 +0000

    flake upate

--- modules/home-manager-modules/kitty.nix
+++ modules/home-manager-modules/kitty.nix
@@ -1,15 +1,19 @@
 # more complex config: seel
 # https://haseebmajid.dev/posts/2023-07-10-setting-up-tmux-with-nix-home-manager/
-{ config, pkgs, userSettings, ...}:
 {
-    programs.kitty = {
-        enable = true;
-	themeFile = "tokyo_night_night";
-        font = {
-            name = userSettings.monospaceFont;
-        };
-        settings = {
-            enable_audio_bell = "no"; # for the love of everything that is holy
-        };
+  config,
+  pkgs,
+  userSettings,
+  ...
+}: {
+  programs.kitty = {
+    enable = true;
+    themeFile = "tokyo_night_night";
+    font = {
+      name = userSettings.monospaceFont;
     };
+    settings = {
+      enable_audio_bell = "no"; # for the love of everything that is holy
+    };
+  };
 }
