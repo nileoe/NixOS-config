ffe90d1fa6afbf53ee2b96f65e535275f402bf8a
nileoe <linoperdrix@gmail.com>
2024-11-27 21:22:58 +0000

    wp change

--- modules/home-manager-modules/shell/scripts/changeWallpaper.nix
+++ modules/home-manager-modules/shell/scripts/changeWallpaper.nix
@@ -1,18 +1,18 @@
 {pkgs}:
 pkgs.writeShellScriptBin "changeWallpaper" ''
 
-# Edit below to control the images transition
-export SWWW_TRANSITION_FPS=60
-export SWWW_TRANSITION_STEP=2
+  # Edit below to control the images transition
+  export SWWW_TRANSITION_FPS=60
+  export SWWW_TRANSITION_STEP=2
 
 
-WPDIR=${../../../../data/wallpapers/selected}
-echo "found wpdir: $WPDIR"
+  WPDIR=${../../../../data/wallpapers/selected}
+  echo "found wpdir: $WPDIR"
 
-read -r wp < <(find $WPDIR -type f \
-    | while read -r wp; do
-    echo "$((RANDOM % 1000)):$wp"
-done \
-    | sort -n | cut -d':' -f2-)
-swww img "$wp"
+  read -r wp < <(find $WPDIR -type f \
+      | while read -r wp; do
+      echo "$((RANDOM % 1000)):$wp"
+  done \
+      | sort -n | cut -d':' -f2-)
+  swww img --resize=fit "$wp"
 ''
