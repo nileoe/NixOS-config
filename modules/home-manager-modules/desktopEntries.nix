f149cb43dc60fb7876bd55c6f3091c6a37660b5b
nileoe <linoperdrix@gmail.com>
2025-08-19 13:58:04 +0200

    added desktop entry for LLM window

--- modules/home-manager-modules/desktopEntries.nix
+++ modules/home-manager-modules/desktopEntries.nix
@@ -3,10 +3,9 @@
   pkgs,
   ...
 }: {
-  firefox = {
-    name = "paprika";
-    genericName = "Web Browser";
-    exec = "firefox %U";
+  xdg.desktopEntries.llm = {
+    name = "llm";
+    exec = "zen --new-window https://gemini.google.com/";
     terminal = false;
     categories = ["Application" "Network" "WebBrowser"];
     mimeType = ["text/html" "text/xml"];
--- todo.md
+++ todo.md
@@ -7,3 +7,5 @@ microsoft-edge
 ## Other
 - remove permitted insecure packages (electron)
 - add auto-garbage collection
+
+do a switch and see what needs correcting
